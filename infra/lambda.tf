b6177754366e618c9280b517c8af3bc877e9a0ad
gucarletto <gucarletto@gmail.com>
2023-04-19 12:02:27 -0300

    turning this into a lambda just for fun

---
+++ infra/lambda.tf
@@ -0,0 +1,30 @@
+resource "aws_lambda_function" "go_blockchain" {
+  function_name = "go_blockchain"
+  runtime = "go1.x"
+  handler = "main"
+  timeout = 10
+  memory_size = 128
+
+  environment = {
+    variables = {
+      PORT = 8080
+    }
+  }
+}
+
+resource "aws_iam_role" "lambda" {
+  name = "lambda-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+      }
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      }
+    ]
+  })
+}
---
+++ infra/variables.tf
@@ -0,0 +1,7 @@
+variable "region" {
+  default = "sa-east-1"
+}
+
+variable "project_name" {
+  default = "terraform_api_gateway"
+}
